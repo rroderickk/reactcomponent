7470a55626c49aa7928c4a89a122b8bc1aea074d
RodrigoMoreno <rroderickk@gmail.com>
2021-12-20 11:10:36 -0500

    readme.md pruebaNodeMachineButtonNeonV1

---
+++ env/botonneon/src/ButtonNeon.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./ButtonNeon.css";
+
+export default function ButtonNeon(props) {
+	const onClickButton = (msg) => {
+		alert(msg);
+	};
+
+	return (
+		<React.Fragment>
+			<div className="ButtonNeonDiv">
+				<header className="Button-header">
+					<button
+						className="ButtonNeon"
+						onClick={() => onClickButton("def")}
+					>
+						this it's a button
+					</button>
+				</header>
+			</div>
+		</React.Fragment>
+	);
+}
